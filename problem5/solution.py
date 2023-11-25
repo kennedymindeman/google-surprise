10166a5860acfcc01cdfb94aeac8a2fa8fc726f6
Ken Mindeman <ken@Kens-MacBook-Pro.local>
2023-11-24 22:28:00 -0600

    Reduce iterations of loop with division
    This seems to optimize the solution enough that it passes all the tests provided by google

--- problem5/solution.py
+++ problem5/solution.py
@@ -2,9 +2,35 @@ def solution(x, y):
     steps = 0
     x = int(x)
     y = int(y)
-    while x != 1 or y != 1:
+
+    while True:
         if x < 1 or y < 1:
             return "impossible"
+
+        if x == 1 or y == 1:
+            return str(steps + max([x, y]) - 1)
+
+        if x < y:
+            quotient = y // x
+            steps += quotient
+            y -= x * quotient
+        else:
+            quotient = x // y
+            steps += quotient
+            x -= y * quotient
+
+
+def simpler_solution(x, y):
+    steps = 0
+    x = int(x)
+    y = int(y)
+
+    while True:
+        if x < 1 or y < 1:
+            return "impossible"
+
+        if x == 1 and y == 1:
+            return steps
 
         if x < y:
             y -= x
@@ -12,5 +38,3 @@ def solution(x, y):
             x -= y
 
         steps += 1
-
-    return str(steps)
--- problem5/test.py
+++ problem5/test.py
@@ -1,4 +1,5 @@
-from solution import solution
+from random import randint, seed
+from solution import simpler_solution, solution
 
 
 def test_solution_already_solved():
@@ -19,3 +20,22 @@ def test_strings():
 
 def test_0s():
     assert solution(0, 1) == "impossible"
+
+
+def test_timeout():
+    assert solution(0, 10 ** 50)
+
+
+def test_impossible():
+    assert solution(2, 4) == "impossible"
+
+
+def test_negative_str():
+    assert solution("-1", "1") == "impossible"
+
+
+def test_large_numbers():
+    seed(1337)
+    for _ in range(10 ** 5):
+        x, y = [randint(0, 10 ** 50)] * 2
+        assert simpler_solution(x, y) == solution(x, y)
